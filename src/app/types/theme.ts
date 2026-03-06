bfd36cb8afb54078ee48bc78ccfdc27fbc9610fe
DanielWilliamParsons <parsonsdwp@gmail.com>
2026-03-06 11:52:44 +0900

    feat: add a global theme provider

--- src/app/types/theme.ts
+++ src/app/types/theme.ts
@@ -1,2 +1,2 @@
-export type ThemePreferences = "light" | "dark" | "system";
+export type ThemePreference = "light" | "dark" | "system";
 export type ResolvedTheme = "light" | "dark";
--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,17 @@ import App from './App.tsx'
 import './index.css'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ThemeProvider } from './app/providers/ThemeProvider';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
-      <ToastContainer position="top-right" autoClose={4000} />
+      <ThemeProvider>
+        <App />
+        <ToastContainer position="top-right" autoClose={4000} />
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
