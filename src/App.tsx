dc2b9adccbb144bff33a9605379bae420f880ac6
DanielWilliamParsons <parsonsdwp@gmail.com>
2026-03-06 09:32:15 +0900

    feat: wire layout components into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
+import ChatInterface from '@/layout/ChatInterface';
+import Header from '@/layout/Header';
+import MainView from '@/layout/MainView';
+import Sidebar from '@/layout/Sidebar';
 
 function App() {
   
 
   return (
     <>
-      
+      <Header />
+      <Sidebar />
+      <MainView />
+      <ChatInterface />
     </>
   )
 }
